1ebd5a1194af9b05eb0d8007d5f4237daa9d83fc
4rthemis <syahrul.uix@gmail.com>
2025-03-08 17:22:00 +0700

    diameter keliling luas lingkaran

---
+++ diameter-keliling-luas-lingkaran.js
@@ -0,0 +1,12 @@
+function lingkaran() {
+  var radius = 5;
+  var diameter = 2 * radius;
+  var keliling = 2 * Math.PI * radius;
+  var luas = Math.PI * radius * radius;
+
+  console.log("Diameter :", diameter);
+  console.log("Circumference: ", keliling);
+  console.log("Area :", luas);
+}
+
+lingkaran();
